6db4f27d8e1c1c33e0316d47184cf9cef8cf694f
acewzj <acewzj>
2022-05-13 00:45:31 +0800

    0512-2

--- mymall-product/src/main/java/com/acewzj/mymall/product/MymallProductApplication.java
+++ mymall-product/src/main/java/com/acewzj/mymall/product/MymallProductApplication.java
@@ -1,8 +1,9 @@
 package com.acewzj.mymall.product;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@MapperScan("com.acewzj.mymall.product.dao")
 @SpringBootApplication
 public class MymallProductApplication {
 
--- mymall-product/src/test/java/com/acewzj/mymall/product/MymallProductApplicationTests.java
+++ mymall-product/src/test/java/com/acewzj/mymall/product/MymallProductApplicationTests.java
@@ -1,13 +1,20 @@
 package com.acewzj.mymall.product;
 
+import com.acewzj.mymall.product.entity.BrandEntity;
+import com.acewzj.mymall.product.service.BrandService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MymallProductApplicationTests {
-
+	@Autowired
+	BrandService brandService;
 	@Test
 	void contextLoads() {
+		BrandEntity brandEntity = new BrandEntity();
+		brandEntity.setName("huawei");
+		brandService.save(brandEntity);
 	}
 
 }
